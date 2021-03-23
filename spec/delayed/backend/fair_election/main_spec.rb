a70f9184209fa7e81c2922c45d8710efd6595e8e
tarvit <>
2021-03-23 15:30:15 +0200

    setup test without ranks implementation

--- spec/delayed/backend/fair_election/main_spec.rb
+++ spec/delayed/backend/fair_election/main_spec.rb
@@ -5,28 +5,53 @@ require "delayed/backend/active_record"
 
 
 describe Delayed::Backend::ActiveRecord::FairSql::Service do
-  describe "behavior" do
-    let(:relation_class) { Delayed::Job.limit(1).class }
-    let(:worker) { instance_double(Delayed::Worker, name: "worker01", read_ahead: 1) }
-    let(:limit) { instance_double(relation_class, update_all: 0) }
-    let(:where) { instance_double(relation_class, update_all: 0) }
-    let(:scope) { instance_double(relation_class, limit: limit, where: where) }
-    let(:job) { instance_double(Delayed::Job, id: 1) }
+  let(:dbms) { "MySQL" }
+  let(:worker) { instance_double(Delayed::Worker, name: "worker-X", read_ahead: 1) }
 
-    before do
-      allow(Delayed::Backend::ActiveRecord::Job.connection).to receive(:adapter_name).at_least(:once).and_return(dbms)
-      Delayed::Backend::ActiveRecord.configuration.reserve_sql_strategy = reserve_sql_strategy
+  before do
+    allow(Delayed::Backend::ActiveRecord::Job.connection).to receive(:adapter_name).at_least(:once).and_return(dbms)
+    Delayed::Backend::ActiveRecord.configuration.reserve_sql_strategy = :fair_sql
+  end
+
+  let!(:jobs) do
+    n = 1
+    {
+      1 => n.delay(fair_id: 'A', locked_at: 5.minutes.ago).to_s,
+      2 => n.delay(fair_id: 'A', locked_at: 5.minutes.ago).to_s,
+      3 => n.delay(fair_id: 'A').to_s,
+
+      4 => n.delay(fair_id: 'B', locked_at: 5.minutes.ago).to_s,
+      5 => n.delay(fair_id: 'B').to_s,
+
+      6 => n.delay(fair_id: 'C').to_s,
+
+      7 => n.delay(fair_id: 'D', locked_at: 5.minutes.ago).to_s,
+      8 => n.delay(fair_id: 'D', locked_at: 5.minutes.ago).to_s,
+
+      9 => n.delay(fair_id: 'E').to_s,
+      10 => n.delay(fair_id: 'E').to_s,
+      11 => n.delay(fair_id: 'E').to_s,
+      12 => n.delay(fair_id: 'E').to_s,
+    }
+  end
+
+  describe 'process Jobs' do
+    let(:processing_order) { [] }
+
+    def process_next_job!
+      job = Delayed::Backend::ActiveRecord::Job.reserve(worker)
+      if job
+        processing_order << jobs.key(job)
+        job.delete
+      end
     end
 
-    context "with reserve_sql_strategy option set to :fair_sql" do
-      let(:dbms) { "MySQL" }
-      let(:reserve_sql_strategy) { :fair_sql }
+    before do
+      jobs.keys.size.times { process_next_job! }
+    end
 
-      it "uses the fair sql version" do
-        allow(Delayed::Backend::ActiveRecord::Job).to receive(:reserve_with_scope_using_optimized_sql)
-        Delayed::Backend::ActiveRecord::Job.reserve_with_scope(scope, worker, Time.current)
-        expect(Delayed::Backend::ActiveRecord::Job).to have_received(:reserve_with_scope_using_optimized_sql).once
-      end
+    it do
+      expect(processing_order).to eq [3, 5, 6, 9, 10, 11, 12]
     end
   end
 end
